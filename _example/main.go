5588d0a76adc7a4b5a2e7ffdb125bb12e31b2d6b
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2019-10-02 20:12:49 +0900

    add example code

---
+++ _example/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/yagi5/tcc"
+)
+
+var (
+	db = &FakeDB{
+		Mutex:  &sync.Mutex{},
+		flight: flight{StockSeatCount: uint64(3)},
+		hotel:  hotel{StockRoomCount: uint64(1)},
+	}
+
+	flightService = tcc.NewService(
+		"flight reservation",
+		db.tryReserveFlightSeat,
+		db.confirmFlightSeatReservation,
+		db.cancelFlightSeat,
+	)
+
+	hotelService = tcc.NewService(
+		"hotel reservation",
+		db.tryReserveHotelRoom,
+		db.confirmHotelRoomReservation,
+		db.cancelHotelRoom,
+	)
+)
+
+type flight struct {
+	StockSeatCount    uint64
+	ReservedSeatCount uint64
+}
+
+type hotel struct {
+	StockRoomCount    uint64
+	ReservedRoomCount uint64
+}
+
+// FakeDB represents a database for example
+type FakeDB struct {
+	*sync.Mutex
+	flight flight
+	hotel  hotel
+}
+
+func (f *FakeDB) tryReserveFlightSeat() error {
+	f.Lock()
+	defer f.Unlock()
+	if f.flight.StockSeatCount == 0 {
+		return fmt.Errorf("no seat")
+	}
+	f.flight.StockSeatCount--
+	return nil
+}
+
+func (f *FakeDB) confirmFlightSeatReservation() error {
+	f.Lock()
+	defer f.Unlock()
+	f.flight.ReservedSeatCount++
+	return nil
+}
+
+func (f *FakeDB) cancelFlightSeat() error {
+	f.Lock()
+	defer f.Unlock()
+	f.flight.StockSeatCount++
+	return nil
+}
+
+func (f *FakeDB) tryReserveHotelRoom() error {
+	f.Lock()
+	defer f.Unlock()
+	if f.hotel.StockRoomCount == 0 {
+		return fmt.Errorf("no room")
+	}
+	f.hotel.StockRoomCount--
+	return nil
+}
+
+func (f *FakeDB) confirmHotelRoomReservation() error {
+	f.Lock()
+	defer f.Unlock()
+	f.hotel.ReservedRoomCount++
+	return nil
+}
+
+func (f *FakeDB) cancelHotelRoom() error {
+	f.Lock()
+	defer f.Unlock()
+	f.hotel.StockRoomCount++
+	return nil
+}
+
+func main() {
+	doFirstReservation(db)
+	doSecondReservation(db)
+}
+
+func doFirstReservation(db *FakeDB) {
+	orchestrator := tcc.NewOrchestrator([]*tcc.Service{flightService, hotelService}, tcc.WithMaxRetries(1))
+	err := orchestrator.Orchestrate()
+	if err != nil {
+		log.Printf("error happened in 1st reservation: %s", err)
+	}
+}
+
+func doSecondReservation(db *FakeDB) {
+	// In second reservation, flight seat is not enough
+	orchestrator := tcc.NewOrchestrator([]*tcc.Service{flightService, hotelService}, tcc.WithMaxRetries(1))
+	err := orchestrator.Orchestrate()
+	if err != nil {
+		log.Printf("error happened in 2nd reservation: %s", err)
+	}
+	tccErr := err.(*tcc.Error)
+	log.Printf("tccErr.Error: %v", tccErr.Error())
+	log.Printf("tccErr.FailedPhase == ErrTryFailed: %v", tccErr.FailedPhase() == tcc.ErrTryFailed)
+	log.Printf("tccErr.ServiceName: %v", tccErr.ServiceName())
+}
